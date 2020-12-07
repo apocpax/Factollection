391487376ed50ec47795354441149489b70c4df8
apocpax <richard.c.lummus90@gmail.com>
2020-12-07 14:26:13 -0800

    fixing

--- express-dev-skills/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'skills to pay the bills' });
-});
-
-module.exports = router;
